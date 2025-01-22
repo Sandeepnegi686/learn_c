11fb5450651a6ba12cef3491c0fc67177da547ab
Sandeep <sandeepnegi686@gmail.com>
2025-01-22 13:13:08 +0530

    added strings

--- 8. Strings/a.c
+++ 8. Strings/a.c
@@ -86,11 +86,96 @@ void printString(char string[]);
 //     return count;
 // }
 
+// int main(){
+//     char str1[] = "hello";
+//     char str2[] = "world";
+//     char empty[] = "";
+//     strcpy(empty, str2);
+//     puts(empty);
+//     return 0;
+// }
+
+// int main(){
+//     char name[100] = "Rahul ";
+//     char lastName[] = "Negi" ;
+//     strcat(name,lastName);
+//     puts(name);
+//     return 0;
+// }
+
+// int main(){
+//     int i = 0;
+//     char ch;
+//     char str[100];
+//     printf("Enter your name : ");
+//     while (ch != '\n'){
+//         scanf("%c", &ch);
+//         str[i] = ch;
+//         i++;
+//     }
+//     str[i] = '\0';
+//     printf("your name is %s",str);
+//     return 0;
+// }
+
+// int main(){
+//     char name[100];
+//     printf("Enter your name : ");
+//     fgets(name,100,stdin);
+//     printf("you name is %s",name);
+//     puts(name);
+//     return 0;
+// }
+
+// void salting(char password[]);
+
+// int main(){
+//     char pass[100];
+//     printf("Enter your password : ");
+//     fgets(pass,100,stdin);
+//     printf("%s",pass);
+//     salting(pass);
+// }
+
+// void salting(char password[]){
+//     char newPassword[104];
+//     char salt[] = "@123";
+//     strcpy(newPassword, password);
+//     strcat(newPassword, salt);
+//     puts(newPassword);
+// }
+
+// void slice(char string[],int n, int m);
+
+// int main(){
+//     char string[] = "Hello World!!!...";
+//     slice(string, 2, 6);
+//     return 0;
+// }
+
+// void slice(char string[], int n, int m){
+//     char newString[m - n + 1];
+//     for(int i = n, j=0; i <= m; i++, j++){
+//         newString[j] = string[i];
+//     }
+//     puts(newString);
+// }
+
+int countVowels(char string[]);
+
 int main(){
-    char str1[] = "hello";
-    char str2[] = "world";
-    char empty[] = "";
-    strcpy(empty, str2);
-    puts(empty);
+    char fullName[] = "my name is sandeep negi";
+    int count = countVowels(fullName);
+    printf("%d",count);
     return 0;
+}
+
+int countVowels(char string[]){
+    int count = 0;
+    for(int i = 0; string[i] != 0; i++){
+        if(string[i] == 'a' || string[i] == 'e' || string[i] == 'i' || string[i] == 'o' || string[i] == 'u'){
+            count++;
+        }
+    }
+    return count;
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learn_c
+
 A repo for learning C language.
+
+Use command for compilation --> "gcc .\a.c"
